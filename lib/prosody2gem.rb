c8327c5e8363491ff54b56d32235bfb10761be4e
Lukas Matt <lukas@zauberstuhl.de>
2016-03-31 18:07:21 +0200

    modified:   lib/prosody2gem.rb

--- lib/prosody2gem.rb
+++ lib/prosody2gem.rb
@@ -1,5 +1,21 @@
+require "fileutils"
+
 class Prosody
+  GEMDIR = Gem::Specification.find_by_name("prosody2gem").gem_dir
+
+  def self.config(opts = {})
+    cfg = "#{GEMDIR}/prosody/etc/prosody/prosody.cfg.lua"
+
+    if opts[:file] && opts[:file] =~ /^\//
+      unless FileUtils.ln_s(opts[:file], cfg, {:force => true})
+        abort("Something went wrong while creating the symlink!")
+      end
+    else
+      abort("Cannot set new configuration file. It has to be absolute!")
+    end
+  end
+
   def self.start
-    puts RbConfig::CONFIG["host_cpu"]
+    system("cd #{GEMDIR}/prosody && bin/prosody")
   end
 end
